767ebab4db1b2094e4569324158b833f203f4b96
aerim.shim <aerim.shim@rd101.co.kr>
2023-11-25 06:10:56 +0900

    thymeleaf 적용

---
+++ src/main/java/com/example/springboot_ex/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.springboot_ex.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+
+        // 컨트롤러 로직 없이 바로 뷰를 리턴하는 경우 ViewController 사용
+        registry.addViewController("/home/home").setViewName("/home/home");
+        registry.addViewController("/member/list").setViewName("/member/member_list");
+    }
+}
