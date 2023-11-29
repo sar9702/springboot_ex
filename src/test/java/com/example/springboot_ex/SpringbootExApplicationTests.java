fdb2b16e0669fa78e27f01cc572b7c1f9caac81b
aerim.shim <aerim.shim@rd101.co.kr>
2023-11-29 14:49:27 +0900

    JPA 적용

---
+++ src/test/java/com/example/springboot_ex/SpringbootExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
